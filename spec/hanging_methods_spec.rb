f9a28a616666b9d0f794db5f6d4cabc0d97784ba
Michael Pearce <michael.pearce@bookrenter.com>
2013-07-31 02:06:44 -0700

    Fix bug to ensure after_invocation search for on the class that has the matching method

--- spec/hanging_methods_spec.rb
+++ spec/hanging_methods_spec.rb
@@ -19,6 +19,9 @@ describe HangingMethods do
       method_name_and_args.size
     end
   end
+
+  class SubTestObject < TestObject
+  end
   
   describe "#hanging_method_invocations" do
     let(:client) { TestObject.new }
@@ -39,17 +42,29 @@ describe HangingMethods do
     end
     
     context "with after_invocation" do
-      let!(:method_1_return) { client.add_more.a_method }
-      let!(:method_2_return) { client.add_more.another_method('arg1', 'arg2') }
-      
-      it "should add invocation" do
-        subject
-        expect(client.add_more_invocations).to eq [[:a_method], [:another_method, 'arg1', 'arg2']]
+      context "with no subclassing" do
+        let!(:method_1_return) { client.add_more.a_method }
+        let!(:method_2_return) { client.add_more.another_method('arg1', 'arg2') }
+        
+        it "should add invocation" do
+          subject
+          expect(client.add_more_invocations).to eq [[:a_method], [:another_method, 'arg1', 'arg2']]
+        end
+
+        it "should return the value of the after_invocation method" do
+          expect(method_1_return).to eq 1
+          expect(method_2_return).to eq 3
+        end
       end
 
-      it "should return the value of the after_invocation method" do
-        expect(method_1_return).to eq 1
-        expect(method_2_return).to eq 3
+      context "with subclass object" do
+        let(:client) { SubTestObject.new }
+        let!(:method_1_return) { client.add_more.a_method }
+
+        it "should add invocation to the hanging method name owner class, not the sublcass" do
+          subject
+          expect(client.add_more_invocations).to eq [[:a_method]]
+        end
       end
     end
   end
